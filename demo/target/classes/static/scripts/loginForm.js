18d80ec5d08ca39e7541992085ce8a632bd65a2b
Frank <strikezone51@gmail.com>
2024-01-13 12:01:57 -0500

    Added additional features

---
+++ demo/target/classes/static/scripts/loginForm.js
@@ -0,0 +1,16 @@
+
+function validate() {
+
+    var username = document.getElementById("inputEmail3").value;
+    var password = document.getElementById("inputPassword3").value;
+
+    if (username == "SampleUser@email.com" && password == "SampleUser") {
+        return true
+    }
+    else {
+        alert("Incorrect username or password.")
+        return false;
+    }
+    
+}
+
